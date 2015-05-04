1f93a352d942b5843d7ac2595b4677b65928d19e
Bob Maple <bobm-git@burner.com>
2015-05-04 10:48:19 -0600

    id_Premultiply initial version

---
+++ id_Premultiply/id_Premultiply.glsl
@@ -0,0 +1,23 @@
+// Premultiply (C)2015 Bob Maple
+// bobm-matchbox@idolum.com
+
+uniform sampler2D  in_front, in_matte;
+uniform     float  adsk_result_w, adsk_result_h;
+uniform       int  op_mode;
+
+//
+
+void main(void) {
+
+    // Convert pixel coords to UV position for texture2D,
+    // fetch the fill and matte pixels and combine them into px
+
+    vec2 uv  = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h );
+    vec4 px  = vec4( texture2D( in_front, uv ).rgb, texture2D( in_matte, uv ).g );
+
+    if( op_mode == 1 )
+      gl_FragColor = vec4( (px.rgb * px.a), px.a );  // Premultiply
+    else
+      gl_FragColor = vec4( (px.rgb / px.a), px.a );  // Unpremultiply
+
+}
