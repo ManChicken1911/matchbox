543d4cbb0781ea369338c1399a3ef3a0869a6b9c
Bob Maple <bobm-git@burner.com>
2016-04-15 01:10:40 -0600

    Added a clamp switch

--- id_Premultiply/id_Premultiply.glsl
+++ id_Premultiply/id_Premultiply.glsl
@@ -5,7 +5,7 @@ uniform sampler2D  in_front, in_matte;
 uniform     float  adsk_result_w, adsk_result_h;
 uniform       int  op_mode;
 uniform      vec3  bg_color;
-
+uniform		 bool  do_clamp;
 //
 
 void main(void) {
@@ -15,10 +15,13 @@ void main(void) {
 
     vec2 uv  = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h );
     vec4 px  = vec4( texture2D( in_front, uv ).rgb, texture2D( in_matte, uv ).g );
+	vec4 npx;
 
-    if( op_mode == 1 )
-      gl_FragColor = vec4( (px.rgb * px.a) + (bg_color * (1.0 - px.a)), px.a );    // Premultiply
-    else
-      gl_FragColor = vec4( ((px.rgb - (bg_color * (1.0 - px.a))) / px.a), px.a );  // Unpremultiply
+    if( op_mode == 1 )    // Premultiply
+      npx = vec4( (px.rgb * px.a) + (bg_color * (1.0 - px.a)), px.a );
 
+    else                  // Unpremultiply
+      npx = vec4( ((px.rgb - (bg_color * (1.0 - px.a))) / px.a), px.a );
+
+	gl_FragColor = do_clamp ? clamp( npx, 0.0, 1.0 ) : npx;
 }
