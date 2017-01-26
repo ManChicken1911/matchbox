02b0b80ab6f00ec811c703514464cfcc9d3be63f
Bob Maple <bobm-git@burner.com>
2017-01-26 11:32:03 -0700

    Added an invert option for hole-cutting instead of cropping

--- id_Croptastic/id_Croptastic.glsl
+++ id_Croptastic/id_Croptastic.glsl
@@ -1,11 +1,11 @@
-// Croptastic (C)2015 Bob Maple
+// Croptastic (C)2017 Bob Maple
 // bobm-matchbox [at] idolum.com
 
 uniform sampler2D  in_front, in_matte;
 uniform	    float  crop_l, crop_r, crop_t, crop_b;
 uniform      vec2  crop_tl, crop_br, offset_xy;
 uniform     float  adsk_result_w, adsk_result_h;
-uniform      bool  border, border_overadv;
+uniform      bool  invert, border, border_overadv;
 uniform      vec3  border_color;
 uniform     float  border_size, border_trans;
 uniform      bool  aborder_l, aborder_r, aborder_t, aborder_b;
@@ -15,7 +15,7 @@ uniform      vec3  aborder_lcolor, aborder_rcolor, aborder_tcolor, aborder_bcolo
 
 // Global variables
 
-vec4 px;
+vec4 npx;
 
 
 //
@@ -24,11 +24,11 @@ void do_border(void) {
 
     if( (gl_FragCoord.x >= crop_l && gl_FragCoord.x <= crop_l + border_size) || (gl_FragCoord.x <= (adsk_result_w - crop_r) && gl_FragCoord.x >= (adsk_result_w - crop_r) - border_size) )
         if( gl_FragCoord.y >= crop_b && gl_FragCoord.y <= adsk_result_h - crop_t )
-            px = vec4( mix( px, vec4( border_color, 1.0 ), (border_trans / 100.0) ) );
+            npx = vec4( mix( npx, vec4( border_color, 1.0 ), (border_trans / 100.0) ) );
 
     if( (gl_FragCoord.y >= crop_b && gl_FragCoord.y <= crop_b + border_size) || (gl_FragCoord.y <= (adsk_result_h - crop_t) && gl_FragCoord.y >= (adsk_result_h - crop_t) - border_size) )
         if( gl_FragCoord.x >= crop_l + border_size && gl_FragCoord.x <= (adsk_result_w - crop_r) - border_size )
-            px = vec4( mix( px, vec4( border_color, 1.0 ), (border_trans / 100.0) ) );
+            npx = vec4( mix( npx, vec4( border_color, 1.0 ), (border_trans / 100.0) ) );
 }
 
 //
@@ -39,23 +39,27 @@ void main(void) {
     // fetch the fill and matte pixels and combine them into px
 
     vec2 uv  = (gl_FragCoord.xy - offset_xy) / vec2( adsk_result_w, adsk_result_h );
-         px  = vec4( texture2D( in_front, uv ).rgb, texture2D( in_matte, uv ).g );
+    vec4 px  = vec4( texture2D( in_front, uv ).rgb, texture2D( in_matte, uv ).g );
+
     vec4 blk = vec4( 0.0, 0.0, 0.0, 0.0 );
+
+    // Initialize this pixel to either the input pixel or black
+
+    npx = invert ? blk : px;
 
     // Do the cropping
 
     if( gl_FragCoord.x < crop_l )
-        px = blk;
+        npx = invert ? px : blk;
     if( gl_FragCoord.x > adsk_result_w - crop_r )
-        px = blk;
+        npx = invert ? px : blk;
 
     if( gl_FragCoord.y > adsk_result_h - crop_t )
-        px = blk;
+        npx = invert ? px : blk;
     if( gl_FragCoord.y < crop_b )
-        px = blk;
+        npx = invert ? px : blk;
 
-
-    // Draw border if it's supposed to be underneath
+    // Draw standard border if it's supposed to be underneath
 
     if( border && !border_overadv )
         do_border();
@@ -66,32 +70,32 @@ void main(void) {
     if( aborder_l ) {
 
         if( (gl_FragCoord.x >= crop_l && gl_FragCoord.x <= crop_l + aborder_lsize) && (gl_FragCoord.y >= crop_b && gl_FragCoord.y <= adsk_result_h - crop_t) )
-            px = vec4( mix( px, vec4( aborder_lcolor, 1.0 ), (aborder_ltrans / 100.0) ) );
+            npx = vec4( mix( npx, vec4( aborder_lcolor, 1.0 ), (aborder_ltrans / 100.0) ) );
     }
 
     if( aborder_r ) {
 
         if( (gl_FragCoord.x <= (adsk_result_w - crop_r) && gl_FragCoord.x >= (adsk_result_w - crop_r) - aborder_rsize) && (gl_FragCoord.y >= crop_b && gl_FragCoord.y <= adsk_result_h - crop_t) )
-            px = vec4( mix( px, vec4( aborder_rcolor, 1.0 ), (aborder_rtrans / 100.0) ) );
+            npx = vec4( mix( npx, vec4( aborder_rcolor, 1.0 ), (aborder_rtrans / 100.0) ) );
     }
 
     if( aborder_t ) {
 
         if( (gl_FragCoord.y <= (adsk_result_h - crop_t) && gl_FragCoord.y >= (adsk_result_h - crop_t) - aborder_tsize) && (gl_FragCoord.x >= crop_l && gl_FragCoord.x <= adsk_result_w - crop_r) )
-            px = vec4( mix( px, vec4( aborder_tcolor, 1.0 ), (aborder_ttrans / 100.0) ) );
+            npx = vec4( mix( npx, vec4( aborder_tcolor, 1.0 ), (aborder_ttrans / 100.0) ) );
     }
 
     if( aborder_b ) {
 
         if( (gl_FragCoord.y >= crop_b && gl_FragCoord.y <= crop_b + aborder_bsize) && (gl_FragCoord.x >= crop_l && gl_FragCoord.x <= adsk_result_w - crop_r) )
-            px = vec4( mix( px, vec4( aborder_bcolor, 1.0 ), (aborder_btrans / 100.0) ) );
+            npx = vec4( mix( npx, vec4( aborder_bcolor, 1.0 ), (aborder_btrans / 100.0) ) );
     }
 
 
-    // Draw border if it's supposed to be on top of
+    // Draw standard border if it's supposed to be on top
 
     if( border && border_overadv )
         do_border();
 
-    gl_FragColor = px;
+    gl_FragColor = npx;
 }
