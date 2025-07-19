bd00de38c76121dc98e5fe2e91896b0574f1cd55
Bob Maple <bobm-git@burner.com>
2025-07-19 13:55:12 -0600

    Added id_VectorVictor which fixes Motion vector passes from Lightwave 3D

---
+++ id_VectorVictor/id_VectorVictor.glsl
@@ -0,0 +1,17 @@
+// VectorVictor (C) 2025 Bob Maple
+// bobm-matchbox [at] idolum.com
+//
+// VER: 2025.07.19
+//
+// Inverts the green channel to make motion vector passes from Lightwave 3D
+// compatible with Motion Blur and StringRay Motion Blur nodes
+
+uniform sampler2D in_front;
+uniform     float adsk_result_w, adsk_result_h;
+
+void main(void) {
+
+ vec4 px = vec4( texture2D( in_front, gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h ) ) );
+ gl_FragColor = vec4( px[0], (px[1] * -1.0)+1.0, px[2], px[3] );
+
+}
